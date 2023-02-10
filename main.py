8d723dfa2b3365d5be1d478648131fd6888a7acf
Logstore <63562913+lobstore@users.noreply.github.com>
2023-02-10 23:16:03 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,96 @@
+import matplotlib.pyplot as plt
+import numpy
+import NeuralNetwork
+# number of input, hidden and output nodes
+input_nodes = 784
+hidden_nodes = 200
+output_nodes = 10
+
+# learning rate
+learning_rate = 0.1
+
+# create instance of neural network
+n = NeuralNetwork.NeuralNetwork(input_nodes, hidden_nodes, output_nodes,
+                                learning_rate)
+#n.cuda()
+# load the mnist training data CSV file into a list
+
+training_data_file = open("mnist_train_100.csv", 'r')
+training_data_list = training_data_file.readlines()
+training_data_file.close()
+# train the neural network
+# epochs is the number of times the training data set is used for training
+epochs = 5
+
+for e in range(epochs):
+    # go through all records in the training data set
+    for record in training_data_list:
+        # split the record by the ',' commas
+        all_values = record.split(',')
+        # scale and shift the inputs
+        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        # create the target output values (all 0.01, except the desired label which is 0.99)
+        targets = numpy.zeros(output_nodes) + 0.01
+        # all_values[0] is the target label for this record
+        targets[int(all_values[0])] = 0.99
+        n.train(inputs, targets)
+        pass
+    pass
+
+test_data_file = open("mnist_test_10.csv", 'r')
+test_data_list = test_data_file.readlines()
+test_data_file.close()
+# test the neural network
+
+# scorecard for how well the network performs, initially empty
+scorecard = []
+
+# go through all the records in the test data set
+for record in test_data_list:
+    # split the record by the ',' commas
+    all_values = record.split(',')
+    # correct answer is first value
+    correct_label = int(all_values[0])
+    # scale and shift the inputs
+    inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+    # query the network
+
+    outputs = n.query(inputs)
+
+    #outputs = n.forward(inputs)
+
+    print(outputs)
+    print("\n Correct is " + str(correct_label) + "\n")
+    # the index of the highest value corresponds to the label
+    label = numpy.argmax(outputs)
+    # append correct or incorrect to list
+
+    if (label == correct_label):
+        # network's answer matches correct answer, add 1 to scorecard
+        scorecard.append(1)
+    else:
+        # network's answer doesn't match correct answer, add 0 to scorecard
+        scorecard.append(0)
+        pass
+
+    pass
+# calculate the performance score, the fraction of correct answers
+scorecard_array = numpy.asarray(scorecard)
+print("performance = ", scorecard_array.sum() / scorecard_array.size)
+
+# run the network backwards, given a label, see what image it produces
+
+# label to test
+label = 5
+# create the output signals for this label
+targets = numpy.zeros(output_nodes) + 0.01
+# all_values[0] is the target label for this record
+targets[label] = 0.99
+print(targets)
+
+# get image data
+image_data = n.backquery(targets)
+
+# plot image data
+plt.imshow(image_data.reshape(28, 28), cmap='Greys', interpolation='None')
+plt.show()
